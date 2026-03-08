b29639dec657c37f934cb612647b917c258d0781
Andrey Yurchuk <r1430@tut.by>
2026-03-08 06:51:45 +0300

    refactor: update auth use cases imports after domain contracts move

--- src/application/auth/login.use-case.ts
+++ src/application/auth/login.use-case.ts
@@ -1,8 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
-import { TokenService } from '../../domain/token.service';
-import { UserRepository } from '../../domain/user.repository';
+import { UserRepository } from '../../domain/repositories/user.repository';
+import { TokenService } from '../../domain/services/token.service';
 import { AuthResponse } from './register.use-case';
 
 export type LoginDto = {
