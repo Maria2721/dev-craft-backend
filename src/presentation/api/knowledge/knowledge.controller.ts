5f03c321489e9f548a5d360ea26cda2c2c04736d
Andrey Yurchuk <r1430@tut.by>
2026-03-20 22:22:29 +0300

    feat: add get topic preview endpoint

--- src/presentation/api/knowledge/knowledge.controller.ts
+++ src/presentation/api/knowledge/knowledge.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { GetKnowledgeTopicsUseCase } from '../../../application/knowledge/get-knowledge-topics.use-case';
+import { GetTopicPreviewUseCase } from '../../../application/knowledge/get-topic-preview.use-case';
 
 @Controller('knowledge/topics')
 export class KnowledgeController {
-  constructor(private readonly getKnowledgeTopicsUseCase: GetKnowledgeTopicsUseCase) {}
+  constructor(
+    private readonly getKnowledgeTopicsUseCase: GetKnowledgeTopicsUseCase,
+    private readonly getTopicPreviewUseCase: GetTopicPreviewUseCase,
+  ) {}
 
   @Get()
   async listTopics() {
     return this.getKnowledgeTopicsUseCase.execute();
+  }
+
+  @Get(':topicId/preview')
+  async getTopicPreview(@Param('topicId') topicId: string) {
+    return this.getTopicPreviewUseCase.execute(topicId);
   }
 }
