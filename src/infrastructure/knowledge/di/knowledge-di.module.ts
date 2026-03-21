e8ce6ae53a02b106f564fbc542a4cad5be345d3d
Andrey Yurchuk <r1430@tut.by>
2026-03-21 20:55:25 +0300

    chore: register topic code tasks use case in knowledge di module

--- src/infrastructure/knowledge/di/knowledge-di.module.ts
+++ src/infrastructure/knowledge/di/knowledge-di.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { GetKnowledgeTopicsUseCase } from '../../../application/knowledge/get-knowledge-topics.use-case';
+import { GetTopicCodeTasksUseCase } from '../../../application/knowledge/get-topic-code-tasks.use-case';
 import { GetTopicPreviewUseCase } from '../../../application/knowledge/get-topic-preview.use-case';
 import { GetTopicQuestionsUseCase } from '../../../application/knowledge/get-topic-questions.use-case';
 import { SubmitTopicQuestionAttemptsUseCase } from '../../../application/knowledge/submit-topic-question-attempts.use-case';
@@ -19,12 +20,14 @@ import { PrismaTopicRepository } from '../../persistence/topic/prisma-topic.repo
     { provide: QuestionRepository, useClass: PrismaQuestionRepository },
     { provide: QuestionAttemptRepository, useClass: PrismaQuestionAttemptRepository },
     GetKnowledgeTopicsUseCase,
+    GetTopicCodeTasksUseCase,
     GetTopicPreviewUseCase,
     GetTopicQuestionsUseCase,
     SubmitTopicQuestionAttemptsUseCase,
   ],
   exports: [
     GetKnowledgeTopicsUseCase,
+    GetTopicCodeTasksUseCase,
     GetTopicPreviewUseCase,
     GetTopicQuestionsUseCase,
     SubmitTopicQuestionAttemptsUseCase,
