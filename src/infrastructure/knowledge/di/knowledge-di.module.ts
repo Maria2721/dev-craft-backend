74fd2033f3e54395a5e2faa5dd8a0748ece31e05
Andrey Yurchuk <r1430@tut.by>
2026-03-21 14:53:41 +0300

    feat: wire topic questions use case in knowledge di module

--- src/infrastructure/knowledge/di/knowledge-di.module.ts
+++ src/infrastructure/knowledge/di/knowledge-di.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { GetKnowledgeTopicsUseCase } from '../../../application/knowledge/get-knowledge-topics.use-case';
 import { GetTopicPreviewUseCase } from '../../../application/knowledge/get-topic-preview.use-case';
+import { GetTopicQuestionsUseCase } from '../../../application/knowledge/get-topic-questions.use-case';
 import { TopicRepository } from '../../../domain/repositories/topic.repository';
 import { PrismaModule } from '../../persistence/prisma/prisma.module';
 import { PrismaTopicRepository } from '../../persistence/topic/prisma-topic.repository';
@@ -12,7 +13,8 @@ import { PrismaTopicRepository } from '../../persistence/topic/prisma-topic.repo
     { provide: TopicRepository, useClass: PrismaTopicRepository },
     GetKnowledgeTopicsUseCase,
     GetTopicPreviewUseCase,
+    GetTopicQuestionsUseCase,
   ],
-  exports: [GetKnowledgeTopicsUseCase, GetTopicPreviewUseCase],
+  exports: [GetKnowledgeTopicsUseCase, GetTopicPreviewUseCase, GetTopicQuestionsUseCase],
 })
 export class KnowledgeDiModule {}
