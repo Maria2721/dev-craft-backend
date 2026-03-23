4bcdfd9d686a2237da6498065f8d3fd24dae4e39
Andrey Yurchuk <r1430@tut.by>
2026-03-23 16:34:01 +0300

    feat: add dify chatflow constants for message requests

---
+++ src/infrastructure/llm/dify-chatflow.constants.ts
@@ -0,0 +1,4 @@
+export const DIFY_CHAT_MESSAGES_PATH = '/chat-messages';
+export const DIFY_CHATFLOW_RESPONSE_MODE_BLOCKING = 'blocking';
+export const DIFY_END_USER_PREFIX = 'devcraft-';
+export const DIFY_INPUT_DEVCRAFT_USER_ID_KEY = 'devcraft_user_id';
